08ddd9c2ecccff1f130b54bb4966c0871c6e7806
Aditya Garg <adityagarg2k14@gmail.com>
2020-12-03 14:52:04 +0530

    UC3

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,19 +5,19 @@ public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		char[] board = createBoard();
-                Scanner userInput = new Scanner(System.in);
-                char userLetter = chooseBoard(userInput);
-                if(userLetter = 'X')
-                {       computerLetter="O";
+                char computerLetter;
+		Scanner userInput = new Scanner(System.in);
+                char userLetter = chooseLetter(userInput);
+                if(userLetter == 'X'){
+		       computerLetter='O';
                 }
-                else if(userLetter=="O")
-                {        computerLetter="X";
+                else if(userLetter == 'O'){
+                       computerLetter= 'X';
                 }
-                else
-                {
-                System.Out.Print("Enter only X or O");
+                else{
+                	System.out.print("Enter only X or O");
                 }
-
+		showBoard(board);
 	}
 	//User Case1
 	private static char[] createBoard() {
@@ -28,8 +28,15 @@ public class TicTacToeGame {
 		return board;
 	}
 	//User Case2
-        private static char chooseBoard(Scanner userInput) {
+        private static char chooseLetter(Scanner userInput) {
                 System.out.print("Enter your option between X or O- ");
-                return userInput.next.charAt(0);
+                return userInput.next().charAt(0);
         }
+	//User Case3
+	private static void showBoard(char[] board) { 
+		for(int i=1;i<10;i++) 
+		{
+		System.out.println(board[i] + "|" +board[i+1] + "|" +board[i+2] );
+		}
+	}
 }

60a6e7f0545bb7f9fae8d2fcf1ccc3fb8656d279
Aditya Garg <adityagarg2k14@gmail.com>
2020-12-03 12:53:16 +0530

    Tic Tac Toe

---
+++ TicTacToeGame.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		char[] board = createBoard();
+	}
+	//User Case1
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i=1; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
+}
